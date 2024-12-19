98f76d60ded4b7997b4b5a814ee628e6b4acc939
Jung Hong Suk <hongsukjung@naver.com>
2024-12-19 16:59:14 +0900

    Step09 MainClass05 에 주석 추가함

--- Step09_AbstractClass/src/test/main/MainClass05.java
+++ Step09_AbstractClass/src/test/main/MainClass05.java
@@ -4,7 +4,7 @@ import test.mypac.Weapon;
 
 public class MainClass05 {
 	public static void main(String[] args) {
-		
+		//익명의 inner class 를 이용해서 Weapon type 의 참조값을 얻어내서 변수에 담은 다음
 		Weapon w1 = new Weapon() {						//{}안에 클래스 class ? extends Weapon{} 
 														//new Weapon() 가로 열고 닫고 Weapon 생성자 호출
 			@Override
@@ -13,8 +13,10 @@ public class MainClass05 {
 				System.out.println("몰라 아무데나 공격~");
 			}
 		};
+		//메소드 호출하면서 전달하기
 		useWeapon(w1);
 		
+		//메소드 호출하면서 즉석에서 Weapon type 의 참조값을 얻어내서 전달하기
 		useWeapon(new Weapon() {
 			//추상클래스 타입 이렇게 쉽게 가능 ~ 익명의 로컬 이너 클래스를 이용해서 추상클래스 원하는 결과 값 얻어내기
 			@Override

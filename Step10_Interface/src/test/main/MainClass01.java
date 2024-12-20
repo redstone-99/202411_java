36ac2fd78b57d0c167dfb3c8dd650a467638ad63
Jung Hong Suk <hongsukjung@naver.com>
2024-12-20 10:17:42 +0900

    Step10 에 MainClass01 추가함

---
+++ Step10_Interface/src/test/main/MainClass01.java
@@ -0,0 +1,17 @@
+package test.main;
+
+import test.mypac.MyRemocon;
+import test.mypac.Remocon;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		/*
+		 * 	Remocon 인터페이스는 data type 의 역할을 할수 있는데
+		 * 	해당 type 의 참조값을 얻어내기 위해서는 인터페이스를 구현한 클래스로
+		 * 	객체 생성을 하면 된다.
+		 */
+		Remocon r1 = new MyRemocon();
+		r1.up();
+		r1.down();
+	}
+}

c357dd3ef6adcf36cf7c3ab8c95700d5208135cd
Jung Hong Suk <hongsukjung@naver.com>
2024-12-20 09:42:10 +0900

    Step10 프로젝트 추가함

---
+++ Step10_Interface/src/test/mypac/MyRemocon.java
@@ -0,0 +1,14 @@
+package test.mypac;
+
+public class MyRemocon implements Remocon{ //인터페이스는 implements 해야 한다
+
+	@Override
+	public void up() {
+		System.out.println("볼륨을 올려요!");
+	}
+
+	@Override
+	public void down() {
+		System.out.println("볼륨을 내려요!");
+	} 
+}
